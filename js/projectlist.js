449f1ab214ccd1198ea6dd3d9a31ec29e1747449
Suzy Hlinka <46274932+suzyhlinka@users.noreply.github.com>
2020-05-02 21:28:59 -0400

    edited files

---
+++ js/projectlist.js
@@ -0,0 +1,5 @@
+var projectTitle= new Object();
+projectTitle.name = "Project Title";
+projectTitle.category = "Category";
+projectTitle.year = "Year";
+projectTitle.thumbnail= "..imgs/project-thumbnails/projectTitle.img";
