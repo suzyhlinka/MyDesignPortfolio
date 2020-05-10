c381ff0133772eb9bdcf3d7c7d2948d28aff79e5
Suzy Hlinka <46274932+suzyhlinka@users.noreply.github.com>
2020-05-10 01:18:20 -0400

    setting up objects

---
+++ Projects/2020/projectlist.js
@@ -0,0 +1,32 @@
+
+
+//var projectTitle= new Object();
+//projectTitle.name = "Project Title";
+//projectTitle.category = "Category";
+//projectTitle.year = "Year";
+//projectTitle.thumbnail= "..imgs/project-thumbnails/projectTitle.img";
+//projectTitle.link = "..Projects/2020/Folder/project.html"
+
+//Categories -
+
+var ChemPal= new Object();
+ChemPal.name = "ChemPal ";
+ChemPal.category = "AR/VR/UX/UI";
+ChemPal.year = "2019";
+ChemPal.thumbnail= "ChemPal/thumbnail.png";
+ChemPal.link = "ChemPal/ChemPalPage.html";
+
+var HPSmart= new Object();
+HPSmart.name = "HP Smart";
+HPSmart.category = "AR/VR/UX/UI", "PPT";
+HPSmart.year = "2019";
+HPSmart.thumbnail="HP/thumbnail.png"
+HPSmart.link = "HP/HPProject.html"
+
+var DigPainting= new Object();
+DigPainting.name = "Digital Painting Recreation";
+DigPainting.category = "Misc";
+DigPainting.year = "2018";
+DigPainting.thumbnail= "DigPainting/thumbnail.png";
+DigPainting.link = "DigPainting/DigPainting.html";
+
--- js/projectlist.js
+++
@@ -1,5 +0,0 @@
-var projectTitle= new Object();
-projectTitle.name = "Project Title";
-projectTitle.category = "Category";
-projectTitle.year = "Year";
-projectTitle.thumbnail= "..imgs/project-thumbnails/projectTitle.img";
